18282ffa80eb5eb74eaba49c4f8d85151b4f0086
Goran Ilic (uniqu3) <uniqu3e@gmail.com>
2020-04-04 15:44:50 +0200

    transforming react shop app to context-api

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
 
-import { store, persistor } from './redux/store';
+import CartProvider from './providers/cart/cart.provider';
 
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <PersistGate persistor={persistor}>
+  <CartProvider>
+      <BrowserRouter>
         <App />
-      </PersistGate>
-    </BrowserRouter>
-  </Provider>,
+      </BrowserRouter>
+  </CartProvider>,
   document.getElementById('root')
 );
